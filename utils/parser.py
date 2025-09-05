c977eb5e239b39221fd11b9c290bbba8b78bb851
yoann <yoann.boget@etu.unige.ch>
2025-09-05 16:48:14 +0200

    Update

--- utils/parser.py
+++ utils/parser.py
@@ -6,7 +6,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--dataset", type=str,
-        default='qm9',
+        default='zinc',
         help="Name of the dataset. Available:  qm9, zinc, planar, sbm"
     )
 
@@ -25,7 +25,7 @@ def parse_args():
 
     parser.add_argument(
         "--wandb", type=str,
-        default='disabled', help="If W&B is online, offline or disabled"
+        default='online', help="If W&B is online, offline or disabled"
     )
 
     parser.add_argument(
