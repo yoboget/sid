569fd55b016be2dd9d654199030cab5232b68d57
yoann <yoann.boget@etu.unige.ch>
2025-09-10 09:08:52 +0200

    Update

--- utils/parser.py
+++ utils/parser.py
@@ -6,7 +6,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--dataset", type=str,
-        default='planar',
+        default='qm9H',
         help="Name of the dataset. Available:  qm9, zinc, planar, sbm"
     )
 
