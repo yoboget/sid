18d3ebf4cb87e3230ced2b3a1988f96f9f6e9ff0
yoann <yoann.boget@etu.unige.ch>
2025-09-10 14:36:55 +0200

    Update

--- eval/stats3.py
+++ eval/stats3.py
@@ -215,7 +215,7 @@ def orca(graph):
     for (u, v) in edge_list_reindexed(graph):
         f.write(str(u) + ' ' + str(v) + '\n')
     f.close()
-    print('path2', tmp_file_path)
+    print('path2', os.path.join(ORCA_DIR, 'orca'))
     output = sp.check_output([os.path.join(ORCA_DIR, 'orca'), 'node', '4', tmp_file_path, 'std'])
     # output = sp.check_output([os.path.join(ORCA_DIR), 'node', '4', tmp_file_path, 'std'])
     print(output)
